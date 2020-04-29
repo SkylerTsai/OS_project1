7c24a5f3c2d88547deb14094b9cc24514d15d329
SkylerTsai <skyler17889@gmail.com>
2020-04-29 17:48:29 +0800

    fixed nanosecond digits

--- kernel_files/p1_printk.c
+++ kernel_files/p1_printk.c
@@ -3,6 +3,6 @@
 
 asmlinkage void sys_p1_printk(int pid, unsigned long ss, unsigned long sns, unsigned long es, unsigned long ens)
 {
-	printk("[project1] %d %lu.%lu %lu.%lu\n", pid, ss, sns, es, ens);
+	printk("[project1] %d %lu.%09lu %lu.%09lu\n", pid, ss, sns, es, ens);
  	return;
 }
